f213bda6c10e364309e2b279678b6cedb8ac9f9e
mathhun <achemer@gmail.com>
2015-12-01 10:01:43 +0900

    inplace editing

--- sqlpp.py
+++ sqlpp.py
@@ -9,10 +9,11 @@ def pprint(reader, writer):
     writer.write(sqlparse.format(reader.read(), reindent=True, keyword_case='upper'))
 
 def pprint_inplace(file):
-    temp = tempfile.NamedTemporaryFile(mode="wb", suffix=".buf")
-    with open(file) as f:
+    temp = tempfile.NamedTemporaryFile(suffix=".sqlpp.tmp", delete=False)
+    with open(file, "r") as f:
         pprint(f, temp)
-    shutil.copyfile(temp.name, file)
+    temp.flush()
+    os.rename(temp.name, file)
     temp.close()
 
 def main():
@@ -20,22 +21,17 @@ def main():
     parser.add_option("-i", "--in-place", action="store_true", default=False)
     (options, files) = parser.parse_args()
 
-    # sqlpp < infile > outfile
     if len(files) == 0:
         pprint(sys.stdin, sys.stdout)
         return
 
     if options.in_place:
-        # sqlpp -i infile1, infile2, ...
         for file in files:
             pprint_inplace(file)
     else:
-        # sqlpp infile1, infile2, ...
         for file in files:
             with open(file) as f:
                 pprint(f, sys.stdout)
-
-    # sqlpp -i dir1, dir2, ...
 
 if __name__ == '__main__':
     main()
